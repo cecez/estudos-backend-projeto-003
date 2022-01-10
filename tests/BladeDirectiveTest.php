13ee0c0055c56528d995357ef68bffc0c7656908
Cezar Rosa <cecez86@gmail.com>
2022-01-10 11:41:01 -0300

    tests: finalizando testes para diretiva Blade e o cacheamento do pacote.

--- tests/BladeDirectiveTest.php
+++ tests/BladeDirectiveTest.php
@@ -7,16 +7,27 @@ use Illuminate\Cache\Repository;
 
 class BladeDirectiveTest extends TestCase2
 {
+    protected RussianCaching $dolly;
+
     /** @test */
     public function it_()
     {
         $bladeDirective = $this->createNewCacheDirective();
+        $isCached = $bladeDirective->setUp($post = $this->makePost());
+        echo '<div>output buffering on agora</div>';
+
+        $this->assertFalse($isCached);
+
+        $cachedFragment = $bladeDirective->tearDown();
+
+        $this->assertEquals('<div>output buffering on agora</div>', $cachedFragment);
+        $this->assertTrue($this->dolly->has($post));
     }
 
     protected function createNewCacheDirective()
     {
         $cache = new Repository(new ArrayStore());
-        $dolly = new RussianCaching($cache);
-        return new BladeDirective($dolly);
+        $this->dolly = new RussianCaching($cache);
+        return new BladeDirective($this->dolly);
     }
 }
